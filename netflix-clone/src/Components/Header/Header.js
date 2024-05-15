1caebda097c5ed177d68e9d9f4f1f48c28e2d579
Mebyo7 <mebyo477@gmail.com>
2024-05-14 23:04:11 -0400

    edited header icon

---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import "../Header/header.css";
+import netflixlogo from "../../assets/images/netflixlogo.png"
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+
+
+const Header = () => {
+  return (
+		<div className="header_outer_container">
+			<div className="header_container">
+				<div className="header_left">
+					<ul>
+						<li>
+							<img src={netflixlogo} width="100" alt="netflix logo" />
+						</li>
+						<li>Netflix</li>
+						<li>Home</li>
+						<li>TVShows</li>
+						<li>Movies</li>
+						<li>Latest</li>
+						<li>MyList</li>
+						<li>Browse by Languages</li>
+					</ul>
+				</div>
+				<div className="header_right">
+					<ul>
+						<li>
+							<SearchIcon />
+						</li>
+						<li>
+							<NotificationsNoneIcon />
+						</li>
+						<li>
+							<AccountBoxIcon />
+						</li>
+						<li>
+							<ArrowDropDownIcon />
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header
